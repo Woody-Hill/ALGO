13b9540221d66ed9b99b4e901f9ba1ce8c55b486
oneul0 <neul.dev@gmail.com>
2026-07-22 09:21:36 +0900

    데일리 - 1문제

--- problem_solve/07-21/[PGS] 네트워크/oneul0.java
+++ problem_solve/07-21/[PGS] 네트워크/oneul0.java
@@ -1,10 +1,41 @@
-import java.io.*;
 import java.util.*;
+class Solution {
+    List<List<Integer>> gr = new ArrayList<>();
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        for(int i = 0; i<n; i++){
+            gr.add(new ArrayList<>());
+        }
+        for(int i = 0; i<computers.length; i++){
+            for(int j = 0; j<computers[i].length; j++){
+                if(i == j) continue;
+                if(computers[i][j] == 1){
+                    gr.get(i).add(j);
+                }
+            }
+        }
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+        int answer = 0;
+        for(int i = 0; i<n; i++){
+            if(visited[i]) continue;
+            answer++;
+            bfs(i);
+        }
+        return answer;
+    }
+    public void bfs(int sx){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(sx);
+        visited[sx] = true;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int next : gr.get(cur)){
+                if(visited[next]) continue;
+                q.offer(next);
+                visited[next] = true;
+            }
+        }
     }
 }
