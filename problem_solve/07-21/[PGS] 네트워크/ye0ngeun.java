264516514fcd8596fd3dcabfbf78cd65934b2e4d
ye0ngeun <121778825+ye0ngeun@users.noreply.github.com>
2026-07-22 13:53:26 +0900

    데일리 - 1문제

--- problem_solve/07-21/[PGS] 네트워크/ye0ngeun.java
+++ problem_solve/07-21/[PGS] 네트워크/ye0ngeun.java
@@ -1,10 +1,27 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i, computers, visited);
+                answer++;
+            }
+        }
+                
+        return answer;
     }
+    
+    void dfs(int current, int[][] arr, boolean[] visited) {
+        visited[current] = true;
+        
+        for (int next = 0; next < arr.length; next++) {
+            if (arr[current][next] == 1 && !visited[next]) {
+                System.out.println(arr[current][next]);
+                dfs(next, arr, visited);
+            }
+        }
+    }
+
 }
