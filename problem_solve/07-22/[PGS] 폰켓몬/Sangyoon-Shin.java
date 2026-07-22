bda4106979b0a2a44d0edc3bb0b8d17ff9739033
Sangyoon-Shin <tlstkddbs223@naver.com>
2026-07-22 20:40:04 +0900

    feat: [PGS] 폰켓몬 풀이 추가

--- problem_solve/07-22/[PGS] 폰켓몬/Sangyoon-Shin.java
+++ problem_solve/07-22/[PGS] 폰켓몬/Sangyoon-Shin.java
@@ -4,7 +4,25 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+        class Solution {
+            public int solution(int[] nums) {
+                Set<Integer> set = new HashSet<>();
+
+                for (int i = 0; i < nums.length; i++){
+                    if (!set.contains(nums[i])){
+                        set.add(nums[i]);
+                    }
+                }
+
+
+                int res = 0;
+                if (set.size() >= nums.length / 2){
+                    res = nums.length / 2;
+                } else {
+                    res = set.size();
+                }
+                return res;
+            }
+        }
     }
 }
