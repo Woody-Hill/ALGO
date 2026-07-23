3d9da9de47bf7bb5fd62494b2dff2d8ca3fa6f50
Woody-Hill <150872127+Woody-Hill@users.noreply.github.com>
2026-07-23 14:43:24 +0900

    feat: [PGS] 타겟 넘버 풀이

--- problem_solve/07-23/[PGS] 타겟 넘버/Woody-Hill.java
+++ problem_solve/07-23/[PGS] 타겟 넘버/Woody-Hill.java
@@ -1,10 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+class Solution {
+    class State {
+        int sum;
+        int idx;
+        
+        State(int sum, int idx) {
+            this.sum = sum;
+            this.idx = idx;
+        }
+    }
+    
+    public int solution(int[] numbers, int target) {
+        int n = numbers.length;
+        
+        Stack<State> stack = new Stack<>();
+        stack.push(new State(0, 0));
+        
+        int answer = 0;
+        
+        while (!stack.isEmpty()) {
+            State current = stack.pop();
+            int currSum = current.sum;
+            int currIdx = current.idx;
+            
+            if (currIdx == n) {
+                if (currSum == target) {
+                    answer += 1;
+                }
+                continue;
+            }
+            
+            stack.push(new State(currSum + numbers[currIdx], currIdx + 1));
+            stack.push(new State(currSum - numbers[currIdx], currIdx + 1));
+        }
+        
+        return answer;
     }
 }
